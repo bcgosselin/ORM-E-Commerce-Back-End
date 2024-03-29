67789f2aebef45068453b7c59bb5b23b2db48f70
bcgosselin <bcgosselin@gmail.com>
2024-03-28 21:23:43 -0500

    updated product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,35 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  // find all products and associated Category and Tag data
+  try {
+    const products = await Product.findAll({ 
+      include: [{ model: Category }, { model: Tag }],
+    });
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+router.get('/:id', async (req, res) => {
+  // find a single product by its `id` be sure to include its associated Category and Tag data
+  try {
+    const product = await Product.findByPk(req.params.id, { 
+      include: [{ model: Category }, { model: Tag }],
+    });
+
+    if (!category) {
+      res.status(404).json({ message: 'Product not found' });
+      return;
+    }
+
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 // create new product
